038ddf6df080c7f05b848d9f4ff3985b62a8351b
yashhasiaj71 <yashhasija71@gmail.com>
2025-07-21 11:06:46 -0500

    to view change

--- app/src/main/java/com/example/demoproject/MainActivity.java
+++ app/src/main/java/com/example/demoproject/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // to view change
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
